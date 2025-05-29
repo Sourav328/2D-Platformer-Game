30a0dd78643d7ca5b96baa3e0a9cb6bf98bc6793
Sourav <souravdhali126@gmail.com>
2025-05-29 14:26:21 +0530

    Add enemy
    Added the first enemy to the game, which patrols on the platforms.
    creating patrolling enemies and implementing the best one.

---
+++ Assets/Script/Enemy_Controller.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Controller : MonoBehaviour
+{
+    [Header("Patrol Settings")]
+    [SerializeField] private float patrolSpeed = 2f;
+    [SerializeField] private Vector3 targetPoint;
+    [SerializeField] private Vector3 pointB;
+    [SerializeField] private Vector3 pointA;
+    [Header("Components")]
+    [SerializeField] private Animator enemyAnimator;
+
+    private void Start()
+    {
+        SetPatrolPoints();
+    }
+
+    private void Update()
+    {
+        PatrolBetweenPoint();
+    }
+    private void SetPatrolPoints()
+    {
+        transform.position = pointA;
+        targetPoint = pointB;
+    }
+
+    private void PatrolBetweenPoint()
+    
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint, patrolSpeed * Time.deltaTime);
+
+        
+        float direction = targetPoint.x - transform.position.x;
+        transform.localScale = new Vector3(Mathf.Sign(direction) * Mathf.Abs
+       (transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+        if (enemyAnimator != null)
+        {
+            enemyAnimator.SetBool("isWalking", true);
+        }
+
+        if (transform.position == targetPoint)
+        {
+            targetPoint = (targetPoint == pointA) ? pointB : pointA;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player_Controller playerController))
+        {
+            playerController.Kill_Player();
+        }
+    }
+}
--- Assets/Script/PickUpKey_Controller.cs
+++ Assets/Script/PickUpKey_Controller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PickUpKey_Controller : MonoBehaviour
 {
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<Player_Controller>() != null)
         {
